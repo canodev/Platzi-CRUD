0f1bda422ac6295d62fa71517e28c35b13cb4c0f
Luis Gomez <canodevjs@gmail.com>
2022-07-28 21:12:07 -0400

    Update and improve the way that we work with our registers, from handled by string's manipulation to using lists

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
+from http import client
 import sys
-clients = 'pablo,ricardo,'
+clients = ['pablo', 'ricardo']
 
 
 def create_client(client_name):
     global clients
 
     if client_name not in clients:
-        clients += client_name  
-        _add_comma()
+        clients.append(client_name)
     else:
         print('Client already is in the client\'s list')
 
 
 def list_clients():
-    global clients
-    print(clients)
+    for idx, client in enumerate(clients):
+        print('{}: {}'.format(idx, client))
 
 
 def print_clients_not_found():
     print('Client is not in clients list.')
 
 
-def update_client(client_name, updated_client_name):
+def update_client(client_name, updated_name):
     global clients
 
     if client_name in clients:
-        clients = clients.replace(client_name + ',', updated_client_name + ',')
+        index = clients.index(client_name)
+        clients[index] = updated_name
     else:
         print_clients_not_found()
 
@@ -34,31 +35,24 @@ def delete_client(client_name):
     global clients
 
     if client_name in clients:
-        clients = clients.replace(client_name + ',', '')
+        clients.remove(client_name)
     else:
        print_clients_not_found()
 
 
 def search_client(client_name):
-    clients_list = clients.split(',')
-
-    for client in clients_list:
+    for client in clients:
         if client != client_name:
             continue
         else:
             return True
 
 
-
-def _add_comma():
-    global clients
-    clients += ','
-
 def _print_welcome():
     print('WELCOME TO PLATZI VENTAS')
     print('*' * 50) 
     print('What would like to do today?')
-    print('[C]reate client')
+    print('[C]pylreate client')
     print('[L]ist clients')
     print('[U]pdate client')
     print('[D]elete client')
@@ -107,6 +101,6 @@ if __name__ == '__main__':
         if found:
             print('The client is in the client\'s list.')
         else:
-            print('The client: {} is not in our client\s list'.format(client_name))
+            print('The client: {} is not in our client\'s list'.format(client_name))
     else:
         print('Invalid command') 
